fb65b610382243b5750e3759759c5ae0dd0f8cfd
Darek Greenly <git@zielak.pl>
2017-07-23 17:34:26 +0200

    something

---
+++ test/timer.js
@@ -0,0 +1,11 @@
+var assert = require('assert')
+
+import Timer from '../src/timer'
+
+describe('Timer', function () {
+
+	let timer
+	beforeEach(function () {
+		timer = new Timer()
+	})
+})
--- test/vector.js
+++ test/vector.js
@@ -3,18 +3,23 @@ var assert = require('assert')
 import Vector from '../src/vector'
 
 describe('Vector', function () {
+
 	let vector
 	beforeEach(function () {
 		vector = new Vector()
 	})
+	
 	describe('#set_xy()', function () {
+
 		it('should apply both x and y', function () {
 			vector.set_xy(1, 1)
 			assert.equal(vector.x, 1, 'x didnt change')
 			assert.equal(vector.y, 1, 'y didnt change')
 		})
+
 		it('should return a vector', function () {
 			assert(vector.set_xy(1, 1) instanceof Vector, `did not return a vector: ${typeof vector.set_xy(1, 1)}`)
 		})
+
 	})
 })
